38d0f81ad3cee7b81e59cdaac0b5457b9508fce6
denis-mahei <denmahei@gmail.com>
2026-07-27 21:13:12 +0300

    feat(client): connecting swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('E-Pharmacy API')
+    .setDescription('Admin Dashboard & Client API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
